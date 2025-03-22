8880ca4bbc79524f9b1fa0b28b8bf96b3f396a3b
JosinaldoGJunior <JosinaldoGJunior@users.noreply.github.com>
2025-03-22 05:04:16 +0000

    deploy: 7109a79c636b485cf4e627386fa350a5ba92e5b1

--- core/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    APP_BASE_URL = "https://barrigarest.wcaquino.me"
-    APP_PORT = "443"  # Porta padrão para HTTPS; use "80" para HTTP
-    APP_BASE_PATH = "/"
-    APP_CONTENT_TYPE = "application/json"
-    MAX_TIMEOUT = 1  # Tempo limite em segundos
-    ENV = "dev"  # Pode ser 'dev', 'staging' ou 'prod'
